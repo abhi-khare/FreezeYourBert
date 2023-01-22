b6372fdbd609a69bbb1476089db14190683b9b48
abhinash khare <khare.abhinash4@gmail.com>
2023-01-22 13:28:16 +0530

    model definition fix: custom model class name clashing with transformer model name

--- scripts/model.py
+++ scripts/model.py
@@ -10,20 +10,19 @@ class TransformerModel(nn.Module):
         super(TransformerModel, self).__init__()
         self.base_model = RobertaModel.from_pretrained("roberta-base")
 
-        self.FC1 = nn.Linear(768, 256)
-        self.drpt = nn.Dropout(0.2)
-        self.FC2 = nn.Linear(256, num_class)
+        self.fc = nn.Sequential(nn.Linear(768, 256),
+                                nn.Dropout(0.2),
+                                nn.Linear(256, num_class))
 
         self.loss_fn = nn.CrossEntropyLoss()
 
     def forward(self, input_ids, attention_mask, label):
         encoded_output = self.base_model(input_ids, attention_mask)
         seq_output = encoded_output[0][:, 0]
-        hidden_output = self.FC1(self.drpt(F.gelu(seq_output)))
-        logits = self.intent_FC2(self.drpt(F.gelu(hidden_output)))
+        logits = self.fc(seq_output)
 
         loss = self.loss_fn(logits, label)
-        label_pred = torch.argmax(nn.Softmax(dim=1)(logits), axis=1)
+        label_pred = torch.argmax(nn.Softmax(dim=1)(logits))
 
         return {
             "loss": loss,
